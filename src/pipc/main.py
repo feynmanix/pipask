f8bf68030ecf312276531e28ace7efbd49bfa092
Feynmanix <feynmanix@users.noreply.github.com>
2025-03-24 22:43:14 +0000

    CLI

--- src/pipc/main.py
+++ src/pipc/main.py
@@ -1,6 +1,52 @@
-def hello_world():
-    return "Hello, World!"
+from dataclasses import dataclass
+from pipc.installers.pip import pip_pass_through
+import sys
+
+import click
+
+# (see relevant pip commands at https://pip.pypa.io/en/stable/cli/pip_install/)
+
+@dataclass
+class ParsedArgs:
+    other_args: list[str]
+    help: bool
+    dry_run: bool
+    report: str | None
+
+    @staticmethod
+    def from_click_context(ctx: click.Context) -> "ParsedArgs":
+        return ParsedArgs(
+            other_args=ctx.args,
+            help=ctx.params["help"],
+            dry_run=ctx.params["dry_run"],
+            report=ctx.params["report"] or None,
+        )
+
+@click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
+@click.option("-h", "--help", is_flag=True)
+@click.option("--dry-run", is_flag=True)
+@click.option("--report", type=str)
+@click.pass_context
+def cli(ctx: click.Context, help: bool, dry_run: bool, report: str) -> None:
+    """pipc - safer python package installation with audit and consent."""
+    all_args = sys.argv[1:]
+    is_install_command = len(ctx.args) > 0 and ctx.args[0] == "install"
+    if not is_install_command or help or dry_run:
+        # Only run when actually installing something
+        pip_pass_through(all_args)
+        return
+    parsed_args = ParsedArgs.from_click_context(ctx)
+    execute_checks(parsed_args)
+    if click.confirm("Do you want to continue?"):
+        pip_pass_through(all_args)
+    else:
+        click.echo("Aborted!")
+        sys.exit(2)
+
+
+def execute_checks(parsed_args: ParsedArgs) -> None:
+    click.echo("Checking stuff...")
 
 
 if __name__ == "__main__":
-    print(hello_world())
+    cli()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,50 @@
-from pipc.main import hello_world
+from typing import Any
 
-def test_hello_world():
-    result = hello_world()
-    assert result == "Hello, World!"
+from pipc.main import cli, ParsedArgs
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "args,expected_positional,expected_options",
+    [
+        (
+            ["--help"],
+            [],  #
+            {"help": True, "dry_run": False, "report": None},
+        ),
+        (
+            ["--dry-run", "--report", "output.txt", "--unknown", "unknown"],
+            ["--unknown", "unknown"],
+            {"help": False, "dry_run": True, "report": "output.txt"},
+        ),
+        (
+            ["--report", "output.txt"],
+            [],  #
+            {"help": False, "dry_run": False, "report": "output.txt"},
+        ),
+        (
+            ["install", "package"],
+            ["install", "package"],  #
+            {"help": False, "dry_run": False, "report": None},
+        ),
+        (
+            ["unknown", "command", "--unknown-option"],
+            ["unknown", "command", "--unknown-option"],
+            {"help": False, "dry_run": False, "report": None},
+        ),
+        (
+            ["install", "--", "--help"],
+            ["install", "--help"],  #
+            {"help": False, "dry_run": False, "report": None},
+        ),
+    ],
+)
+def test_parses_cli_args(args: list[str], expected_positional: list[str], expected_options: dict[str, Any]):
+    ctx = cli.make_context("pipc", args)
+    parsed_args = ParsedArgs.from_click_context(ctx)
+    assert parsed_args.other_args == expected_positional
+    assert parsed_args.help == expected_options["help"]
+    assert parsed_args.dry_run == expected_options["dry_run"]
+    assert parsed_args.report == expected_options["report"]
+
