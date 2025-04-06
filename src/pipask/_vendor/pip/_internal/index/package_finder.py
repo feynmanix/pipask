0f6090456ede2c8c6af23d28c96c90a756c3d6ea
Feynmanix <feynmanix@users.noreply.github.com>
2025-04-06 19:07:49 +0100

    More files from forked pip

--- src/pipask/_vendor/pip/_internal/index/package_finder.py
+++ src/pipask/_vendor/pip/_internal/index/package_finder.py
@@ -37,7 +37,7 @@ from pipask._vendor.pip._internal.utils.packaging import check_requires_python
 from pipask._vendor.pip._internal.utils.unpacking import SUPPORTED_EXTENSIONS
 
 if TYPE_CHECKING:
-    from pip._vendor.typing_extensions import TypeGuard
+    from typing_extensions import TypeGuard
 
 __all__ = ["FormatControl", "BestCandidateResult", "PackageFinder"]
 
