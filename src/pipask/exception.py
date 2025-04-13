c37bc3b8924ac65f87bc0e4b9f024ad942dc44fd
Feynmanix <feynmanix@users.noreply.github.com>
2025-04-14 00:42:51 +0100

    Ask for consent before executing 3rd party code

--- src/pipask/exception.py
+++ src/pipask/exception.py
@@ -6,15 +6,7 @@ class HandoverToPipException(PipaskException):
     pass
 
 
-class PipAskResolutionException(PipaskException):
-    """Exception raised when pipask cannot resolve the dependencies with a given method"""
-
-    def __init__(self, message: str):
-        super().__init__(message)
-        self.message = message
-
-
-class PipAskCodeExecutionDeniedException(PipAskResolutionException):
+class PipAskCodeExecutionDeniedException(PipaskException):
     """Exception raised when we are not allowed execute 3rd party code in a package"""
 
     def __init__(self, message: str):
